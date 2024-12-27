1ff81f3b68e6fe7675044e24c9b01a0ef887b447
BRUNELLIERE Erwan <ebrunelliere@softwaymedical.fr>
2024-12-27 17:40:59 +0100

    correction

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cp ./oh-my-zsh/agnoster-custom.zsh-theme ~/.oh-my-zsh/themes/agnoster-custom.zsh-theme
 cp ./oh-my-zsh/agnoster-everforest.zsh-theme ~/.oh-my-zsh/themes/agnoster-everforest.zsh-theme
 cp ./btop/purple-dark-sea.theme ~/.config/btop/themes
-cp ./foor.ini ~/.config/foot/foot.ini
+cp ./foot.ini ~/.config/foot/foot.ini
