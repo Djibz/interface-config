3c2470a8a130b3d1dc9c58154ec59b5b88723b26
BRUNELLIERE Erwan <ebrunelliere@softwaymedical.fr>
2024-12-27 17:34:57 +0100

    everforest

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 cp ./oh-my-zsh/agnoster-custom.zsh-theme ~/.oh-my-zsh/themes/agnoster-custom.zsh-theme
+cp ./oh-my-zsh/agnoster-everforest.zsh-theme ~/.oh-my-zsh/themes/agnoster-everforest.zsh-theme
 cp ./btop/purple-dark-sea.theme ~/.config/btop/themes
